e336a05efd63478299f964346110931ddadfffa7
subramaniyan <subramaniyanm@xbi4.com>
2023-08-20 09:35:50 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports ={
+    "env": {
+        "es2021": true,
+        "node": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "airbnb-base",
+        "plugin:@prettier/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import'/typescript",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        ecmaversion:12,
+        sourceType:"module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "import",
+        "prettier"
+    ],
+    "rules": {
+        "prettier/prettier": "error",
+        "import/extensions": "off",
+        "no-console": "off",
+        "import/order":[
+            "error",
+            {
+                "newlines-between":"never",
+                groups:[
+                    ['builtin','external'],
+                    ['internal','parent','sibling','index']
+                ]
+            }
+        ]
+    },
+    "settings": {
+        "import/resolver": {
+            "typescript": {
+                "alwaysTryTypes": true,
+                "project": "./tsconfig.json"
+            }
+        }
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { addition } from "./Math/Math"
+
+console.log("Helloworld")
+
+console.log(addition(2,10))
